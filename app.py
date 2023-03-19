b6e9b82b875c9eb427471065a788e9ca20ad9590
swaraj <swarajkkd@gmail.com>
2023-03-20 00:34:53 +0530

    Added html page and modified backend

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import pickle
 import flask
-from flask import Flask, request, app, jsonify
+from flask import Flask, request, app, jsonify, render_template
 from flask import Response
 from flask_cors import CORS
 import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/predict/postman", methods=['POST'])
 def predict():
     if(request.method=='POST'):
         f1 = int(request.json['Frequency'])
@@ -22,6 +26,23 @@ def predict():
         result = pickled_model.predict(testing_set.reshape(1,-1))
 
         return jsonify(f"The pressure level is {result} decibels.")
+        
+
+@app.route("/predict", methods=['POST'])
+def predictForm():
+    if(request.method=='POST'):
+        f1 = int(request.form['Frequency'])
+        f2 = float(request.form['Angle_of_attack'])
+        f3 = float(request.form['Chord_length'])
+        f4 = float(request.form['F-S_velocity'])
+        f5 = float(request.form['Suction_side'])
+
+        result = 0
+        testing_set = np.array([f1,f2,f3,f4,f5])
+        pickled_model = pickle.load(open('model.pkl','rb'))
+        result = pickled_model.predict(testing_set.reshape(1,-1))
+
+        return render_template("index.html", result = result)
 
 
 if __name__=="__main__":
